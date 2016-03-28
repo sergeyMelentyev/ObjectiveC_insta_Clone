8c47734e27e1e50ab98213124af3aeb70e9e014a
Sergey Melentyev <sergey.melentyev@gmail.com>
2016-03-28 16:02:13 +0300

    new version

---
+++ mainPageViewController.h
@@ -0,0 +1,13 @@
+//
+//  mainPageViewController.h
+//  ObjectiveC_insta_Clone
+//
+//  Created by Админ on 28.03.16.
+//  Copyright © 2016 Melentyev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface mainPageViewController : UIViewController
+
+@end
---
+++ mainPageViewController.m
@@ -0,0 +1,13 @@
+//
+//  mainPageViewController.m
+//  ObjectiveC_insta_Clone
+//
+//  Created by Админ on 28.03.16.
+//  Copyright © 2016 Melentyev. All rights reserved.
+//
+
+#import "mainPageViewController.h"
+
+@implementation mainPageViewController
+
+@end
